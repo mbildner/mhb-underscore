50e55f8304eb4370ccd84f656aa99860e54d336b
Moshe Bildner <moshe.bildner@gmail.com>
2014-08-01 09:57:40 -0400

    Type testers finished.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+	return gulp.src([
+		'./*.js',
+		'!./*spec.js',
+		])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+})
+
+
+gulp.task('default', []);
